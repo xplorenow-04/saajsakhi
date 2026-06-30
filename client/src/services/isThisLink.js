7dfa3183b5387b131003726e6e6c7c19549bd598
xplorenow-04 <xplorenow04@gmail.com>
2026-06-30 13:23:32 +0530

    Delete client directory

--- client/src/services/isThisLink.js
+++
@@ -1,7 +0,0 @@
-export const isThisLink = (msg="")=>{
-    let message = msg.toLowerCase()
-    if(message.includes("https:") || message.includes("http:") || message.includes(".com") || message.includes(".in") || message.includes(".dev") ){
-        return true
-    }
-    return false
-}
--- client/src/socket/socket.js
+++
@@ -1,9 +0,0 @@
-import {io} from 'socket.io-client';
-
-const BACKEND_URL = import.meta.env.VITE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL_PROD : import.meta.env.VITE_BACKEND_URL;
-
-export const socket = io(BACKEND_URL,{
-    withCredentials:true,
-})
-
-
